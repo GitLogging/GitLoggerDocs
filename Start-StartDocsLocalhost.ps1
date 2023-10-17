9014bfbc96dde835ecc558f30c48e35c120ce4f0
Jake Bolton <ninmonkeys@gmail.com>
2023-10-17 14:02:05 -0500

    new default jekyll build arguments

--- Start-StartDocsLocalhost.ps1
+++ Start-StartDocsLocalhost.ps1
@@ -1,4 +1,4 @@
-
+using namespace System.Collections.Generic
 
 <#
 .SYNOPSIS
@@ -13,21 +13,30 @@
         > pushd 'GitLoggerDocs'
         > . ./Start-StartDocsLocalhost.ps1
 #>
-
 if(-not (gcm 'bundle' -ea ignore)) {
     # $Env:PATH += ([IO.Path]::PathSeparator), (gi -ea stop 'C:\Ruby32-x64\bin') -join ''
     $Env:Path = $Env:Path, 'C:\Ruby32-x64\bin' -join ([IO.Path]::PathSeparator)
 }
 
-$Jekyll_BuildCfg ??= @{
+
+$Jekyll_BuildCfg = @{
     LiveReload = $true
     AutoOpen   = $True
-    DocsRoot = 'H:/data/2023/my_git/GitLoggerDocs'
+    DocsRoot = gi 'H:/data/2023/my_git/GitLoggerDocs' # base dir for jekyll source
+    # current filepath: H:/data/2023/my_git/GitLoggerDocs/Start-StartDocsLocalhost.ps1
+    Watch = $true
+    IncrementalBuild = $true
+    JekyllEnv = 'development' # 'production'
+    Host = 'http://localhost'
+    # Host = 'localhost'
+    Port = '4000'
+    StartingUrlPath = 'http://localhost:4000/GitLogger-Metrics'
 }
+
 "Using `$Jekyll_BuildCfg in `"$PSCommandPath`"" | write-verbose -verb
 $Jekyll_BuildCfg | ft -auto
 
-$BinBundle = Get-Command -CommandType Application -Name 'bundle' -ea 'continue'
+$BinBundle = Get-Command -CommandType Application -Name 'bundle' -ea 'stop' -TotalCount 1
 if( ! $binBundle ) {
     throw 'Ruby binary "bundle" was not found in $Env:Path !'
 }
@@ -40,6 +49,7 @@ if(-not (Test-Path $Jekyll_BuildCfg.StaticSiteRoot)) {
     mkdir -Path $Jekyll_BuildCfg.StaticSiteRoot
 }
 
+Pushd -stack 'jekyll'
 Push-Location -stack 'jekyll' $Jekyll_BuildCfg.StaticSiteRoot
 
 # others: '--incremental'
@@ -48,14 +58,45 @@ if ($Jekyll_BuildCfg.AutoOpen) {
     Start-Process -FilePath 'http://localhost:4000'
 }
 
-if ($Jekyll_BuildCfg.LiveReload) {
-    # else: & $BinBundle @('exec', 'jekyll', 'serve', '--livereload')
-    #throws: 'bundle.bat bundle' is not recognized as a name of a cmdlet,
-    & 'Bundle' @('exec', 'jekyll', 'serve', '--livereload')
-}
-else {
-    &  'Bundle' @('exec', 'jekyll', 'serve')
-}
+'Jekyll "bundle" Build Options: <https://jekyllrb.com/docs/configuration/options/>' | write-verbose -verbose
+# build site, defaults to live reload 2023-10-01
+[List[Object]]$binArgs = @()
+
+#  function GLDocs.Jekyll.BuildArgs {
+    $binArgs.AddRange(@(
+        'exec', 'jekyll', 'serve' ))
+
+    if( $Jekyll_BuildCfg.LiveReload) {
+         $binArgs.AddRange(@(
+            '--livereload' ))
+    }
+    $binArgs.AddRange(@(
+        $Jekyll_BuildCfg.Watch ?
+            '--watch' :
+            '--no-watch' ))
+
+    $binArgs.AddRange(@(
+        '--port',
+            $Jekyll_BuildCfg.Port ?
+            $Jekyll_BuildCfg.Port : '4000'
+        '--host',
+            $Jekyll_BuildCfg.Host ?
+            $Jekyll_BuildCfg.Host : 'http://localhost'
+        ))
+
+    if($Jekyll_BuildCfg.IncrementalBuild) {
+        $binArgs.AddRange(@(
+            '--incremental' ))
+    }
+
+# $binArgs | Join-String -sep ' ' -op 'bundleArgs: ' | Dotils.Write-DimText | Infa
+$binArgs
+    | Join-String -sep ' ' -op 'InvokeNativeCommand: bundle => '
+    | Join-String -op "${fg:gray60}${bg:gray20}"
+    | Write-verbose -verbose
+
+& $BinBundle @binArgs
+
 
 
 Pop-Location -stack 'jekyll'
