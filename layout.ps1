2a3a816f5afd0ea0cd1ffcb8c28bee06bc826041
James Brundage <+@noreply.github.com>
2026-05-01 12:58:03 -0700

    fix: Using root relative paths in layout

--- layout.ps1
+++ layout.ps1
@@ -34,8 +34,8 @@ $HtmlHeader  = @"
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>${PageTitle}</title>
     <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Comfortaa">
-    <link rel="stylesheet" href="./css/base.css">
-    <script src="js/highlight.min.js"></script>
+    <link rel="stylesheet" href="/css/base.css">
+    <script src="/js/highlight.min.js"></script>
     <script>hljs.highlightAll();</script>
 </head>
 
